a82186e2ee4d66b7ebc06d804a4efb25fe8a3eb1
GUSTAVOAR <GUSTAVOAR@GUSTAVO>
2017-05-28 19:30:53 -0300

    Implementação EmprestimoDAO

--- BibliotecaFit/src/br/com/impacta/dao/EmprestimoDAO.java
+++ BibliotecaFit/src/br/com/impacta/dao/EmprestimoDAO.java
@@ -1,5 +1,115 @@
 package br.com.impacta.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+import br.com.impacta.model.Emprestimo;
+import br.com.impacta.sql.ConnectionFactory;
+
 public class EmprestimoDAO {
+
+	private Connection connection;
+
+	public EmprestimoDAO() {
+		this.connection = new ConnectionFactory().getConnection();
+	}
+
+	public void adiciona(Emprestimo emprestimo) {
+
+		String sql = "INSERT INTO tb_emprestimos (idEmprestimo,numExemplar,dataEmprestimo,dataPrevistaRetorno) VALUES (?,?,?,?)";
+
+		try {
+			// prepared statement para inserção
+			PreparedStatement stmt = connection.prepareStatement(sql);
+
+			// seta os valores
+			stmt.setLong(1, emprestimo.getIdEmprestimo());
+			stmt.setLong(2, emprestimo.getNumExemplar());
+			stmt.setDate(3, (java.sql.Date) new Date(emprestimo.getDataEmprestimo().getTimeInMillis()));
+			stmt.setDate(4, (java.sql.Date) new Date(emprestimo.getDataPrevistaRetorno().getTimeInMillis()));
+
+			// executa
+			stmt.executeQuery();
+			stmt.close();
+
+		} catch (SQLException e) {
+			throw new RuntimeException();
+		}
+
+	}
+
+	public void altera(Emprestimo emprestimo) {
+
+		String sql = "UPDATE tb_emprestimos set numExemplar=?,dataEmprestimo=?, dataPrevistaRetorno=? where idEmprestimo=?";
+
+		try {
+			// prepared statement para inserção
+			PreparedStatement stmt = connection.prepareStatement(sql);
+
+			// seta os valores
+			stmt.setLong(1, emprestimo.getNumExemplar());
+			stmt.setDate(2, (java.sql.Date) new Date(emprestimo.getDataEmprestimo().getTimeInMillis()));
+			stmt.setDate(3, (java.sql.Date) new Date(emprestimo.getDataPrevistaRetorno().getTimeInMillis()));
+
+			// executa
+			stmt.executeQuery();
+			stmt.close();
+
+		} catch (SQLException e) {
+			throw new RuntimeException();
+		}
+
+	}
+
+	public void remove(Emprestimo emprestimo) {
+
+		try {
+			PreparedStatement stmt = connection.prepareStatement("delete from tb_emprestimos where idEmprestimo=?");
+
+			stmt.setLong(1, emprestimo.getIdEmprestimo());
+
+			stmt.executeQuery();
+			stmt.close();
+
+		} catch (SQLException e) {
+			throw new RuntimeException();
+		}
+
+	}
+
+	public ArrayList<Emprestimo> getEmprestimos() {
+
+		try {
+			ArrayList<Emprestimo> emprestimos = new ArrayList<Emprestimo>();
+			PreparedStatement stmt = this.connection.prepareStatement("select * from tb_emprestimos");
+			ResultSet rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				// criando o objeto Editora
+				Emprestimo emprestimo = new Emprestimo();
+				emprestimo.setIdEmprestimo(rs.getLong("idEmprestimo"));
+				emprestimo.setNumExemplar(rs.getLong("numExemplar"));
+
+				// montando a data de emprestimo através do Calendar
+				Calendar data = Calendar.getInstance();
+				data.setTime(rs.getDate("dataEmprestimo"));
+				emprestimo.setDataEmprestimo(data);
+
+				// adicionando o objeto à lista
+				emprestimos.add(emprestimo);
+
+			}
+			rs.close();
+			stmt.close();
+			return emprestimos;
+		} catch (SQLException e) {
+			throw new RuntimeException();
+		}
+	}
 
 }
--- BibliotecaFit/src/br/com/impacta/model/Emprestimo.java
+++ BibliotecaFit/src/br/com/impacta/model/Emprestimo.java
@@ -4,38 +4,57 @@ import java.util.Calendar;
 
 public class Emprestimo {
 
+	private long idEmprestimo;
 	private long idTipoPessoa;
 	private long idPessoa;
 	private long numExemplar;
 	private Calendar dataEmprestimo;
 	private Calendar dataPrevistaRetorno;
+
+	public long getIdEmprestimo() {
+		return idEmprestimo;
+	}
+
+	public void setIdEmprestimo(long idEmprestimo) {
+		this.idEmprestimo = idEmprestimo;
+	}
+
 	public long getIdTipoPessoa() {
 		return idTipoPessoa;
 	}
+
 	public void setIdTipoPessoa(long idTipoPessoa) {
 		this.idTipoPessoa = idTipoPessoa;
 	}
+
 	public long getIdPessoa() {
 		return idPessoa;
 	}
+
 	public void setIdPessoa(long idPessoa) {
 		this.idPessoa = idPessoa;
 	}
+
 	public long getNumExemplar() {
 		return numExemplar;
 	}
+
 	public void setNumExemplar(long numExemplar) {
 		this.numExemplar = numExemplar;
 	}
+
 	public Calendar getDataEmprestimo() {
 		return dataEmprestimo;
 	}
+
 	public void setDataEmprestimo(Calendar dataEmprestimo) {
 		this.dataEmprestimo = dataEmprestimo;
 	}
+
 	public Calendar getDataPrevistaRetorno() {
 		return dataPrevistaRetorno;
 	}
+
 	public void setDataPrevistaRetorno(Calendar dataPrevistaRetorno) {
 		this.dataPrevistaRetorno = dataPrevistaRetorno;
 	}
